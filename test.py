c8be1a26ba5dc03ac541cc862d9fe869834ee620
donghan <donghan@ufl.edu>
2022-09-08 23:10:21 -0400

    new

--- test.py
+++ test.py
@@ -1,42 +1,37 @@
-# This example formulates and solves the following simple MIP model:
-#  maximize
-#        x +   y + 2 z
-#  subject to
-#        x + 2 y + 3 z <= 4
-#        x +   y       >= 1
-#        x, y, z binary
+# HomeWork1 Question 1.1
+# Created by Dong Han
+# Solving a integer program by using Python interface to Gurobi 
+
 
 import gurobipy as gp
 from gurobipy import GRB
 
-try:
+# Create a new model
+m = gp.Model("hw1")
 
-    # Create a new model
-    m = gp.Model("hw1")
+# Create variables
+x1 = m.addVar(vtype=GRB.INTEGER, name="x1")
+x2 = m.addVar(vtype=GRB.INTEGER, name="x2")
 
-    # Create variables
-    x1 = m.addVar(vtype=GRB.BINARY, name="x1")
-    x2 = m.addVar(vtype=GRB.BINARY, name="x2")
+# Set objective
+m.setObjective(x1 + 0.64 * x2, GRB.MAXIMIZE)
 
-    # Set objective
-    m.setObjective(x1 + 0.64 * x2, GRB.MAXIMIZE)
+# Add constraint 1
+m.addConstr(50 * x1 + 31 * x2 <= 250, "c1")
 
-    # Add constraint
-    m.addConstr(50 * x1 + 31 * x2 <= 250, "c0")
+# Add constraint 2
+m.addConstr(3 * x1 - 2 * x2 >= -4, "c2")
 
-    # Add constraint
-    m.addConstr(3 * x1 - 2 * x2 >= -4, "c1")
+# Add constraints 3
+m.addConstr(x1 >= 0, "c3")
 
-    # Optimize model
-    m.optimize()
+# Add constraints 4
+m.addConstr(x2 >= 0, "c4")
 
-    for v in m.getVars():
-        print('%s %g' % (v.VarName, v.X))
+# Optimize model
+m.optimize()
 
-    print('Obj: %g' % m.ObjVal)
+for val in m.getVars():
+    print('%s %g' % (val.VarName, val.X))
 
-except gp.GurobiError as e:
-    print('Error code ' + str(e.errno) + ': ' + str(e))
-
-except AttributeError:
-    print('Encountered an attribute error')
+print('Obj: %g' % m.ObjVal)
